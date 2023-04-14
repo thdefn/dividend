206049b738c0204d18ffb1cca563254e94eaaa06
haden <80521474+thdefn@users.noreply.github.com>
2023-04-14 15:34:34 +0900

    엔티티 구현

---
+++ src/main/java/org/example/dividend/persist/DividendRepository.java
@@ -0,0 +1,9 @@
+package org.example.dividend.persist;
+
+import org.example.dividend.persist.entity.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+}
