80c16ce15e88ac1c2be5d7d7b3603777ff2fd218
haden <80521474+thdefn@users.noreply.github.com>
2023-04-14 14:08:30 +0900

    Initial commit

---
+++ src/main/java/org/example/dividend/DividendApplication.java
@@ -0,0 +1,13 @@
+package org.example.dividend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DividendApplication.class, args);
+    }
+
+}
