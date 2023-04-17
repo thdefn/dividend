1965a34389ce627276c6640e6f309618369c64aa
haden <80521474+thdefn@users.noreply.github.com>
2023-04-17 18:20:54 +0900

    JWT

--- src/main/java/org/example/dividend/security/TokenProvider.java
+++ src/main/java/org/example/dividend/security/TokenProvider.java
@@ -1,4 +1,59 @@
 package org.example.dividend.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+    private static final long TOKEN_AVAILABLE_TIME = 1000 * 60 * 60; // 한시간
+
+    private static final String KEY_ROLES = "roles";
+
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        var now = new Date();
+        var expireDate = new Date(now.getTime() + TOKEN_AVAILABLE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) // 토큰 생성 시간
+                .setExpiration(expireDate) //토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
+                .compact();
+    }
+
+    public String getUsername(String token){
+        return this.parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token){
+        if(!StringUtils.hasText(token)) return false;
+        var claims = this.parseClaims(token);
+        return !claims.getExpiration().before(new Date()); // 토큰의 만료시간이 현재보다 이전이 아닐때 true
+    }
+
+    private Claims parseClaims(String token){
+        try {
+            return Jwts.parser().setSigningKey(this.secretKey)
+                    .parseClaimsJws(token) //jwt string에서 클레임 추출해라
+                    .getBody();
+        }catch (ExpiredJwtException e){
+            // TODO
+            return e.getClaims();
+        }
+    }
 }
